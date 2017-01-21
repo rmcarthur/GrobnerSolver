e3f401662446ce9cb6e2a34fa0f9d45e1ae322a1
Rex McArthur <rex@byu.edu>
2017-01-21 15:07:48 -0700

    CHanged Readme edited setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from distutils.core import setup
-try:
-    long_descrip = open('README.txt').read()
+
 setup(
     name='GroebnerSolver',
-    description='Computer Algebra solver for Groebner basis'
+    description='Computer Algebra solver for Groebner basis',
     version='0.1',
     author='Rex McArthur',
     author_email='drexmcarthur@gmail.com',
@@ -13,7 +12,10 @@ setup(
     classifiers=['Development Status :: 3 -Alpha', 
                 'Programming Language :: Python :: 2.7'],
 
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
+    url='https://github.com/drexmca/GrobnerSolver',
+    requirements=['numpy','pandas', 'scipy'],
+
 )
 
 
