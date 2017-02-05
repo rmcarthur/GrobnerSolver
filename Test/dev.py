334901540fff69ada9f8a3f9d6163045bfc1938f
Rex McArthur <rex@byu.edu>
2017-02-04 21:05:28 -0700

    Worked on test method for solve in Grobner Basis

--- Test/dev.py
+++ Test/dev.py
@@ -9,11 +9,6 @@ from GrobnerSolver.grobner import maxheap
 
 
 
-#a1 = np.arange(3**3).reshape(3,3,3) 
-#a2 = np.arange(3**3).reshape(3,3,3) 
-#a1[0,0,0] = 7
-#a2[1,1,1] = 9
-
 a1 = np.arange(2**2).reshape(2,2)
 a2 = np.array([[2,2],[2,2]])
 a3 = np.array([[1,0],[2,0]])
@@ -22,11 +17,19 @@ c2 = MultiCheb(a2)
 c3 = MultiCheb(a3)
 c_list = [c1, c2, c3]
 grob = Grobner(c_list)
-grob.add_s_to_matrix()
+grob.solve()
+
+
+
+
+
+
+
+#grob.add_s_to_matrix()
 #print(grob.label)
 #print('mat')
 #print(grob.matrix)
-grob.add_r_to_matrix()
+#grob.add_r_to_matrix()
 #print(grob.matrix)
 #
 #a3 = np.array([[0,0],[0,1]])
